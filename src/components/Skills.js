a65058d1ea65923e2759464f895582be8172315c
LanaVol <svolosh4yk@gmail.com>
2023-07-28 22:33:47 +0300

    update for responsive

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 const Skill = ({ name, x, y }) => {
   return (
     <motion.div
-      className="flex items-center justify-center rounded-full font-semibold bg-dark text-light py-3 px-6 shadow-dark cursor-pointer absolute dark:text-dark dark:bg-light"
+      className="flex items-center justify-center rounded-full font-semibold bg-dark text-light py-3 px-6 shadow-dark cursor-pointer absolute dark:text-dark dark:bg-light lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3 xs:bg-transparent xs:dark:bg-transparent xs:text-dark xs:dark:text-light xs:font-bold"
       whileHover={{ scale: 1.05 }}
       initial={{ x: 0, y: 0 }}
       whileInView={{ x: x, y: y, transition: { duration: 1.5 } }}
@@ -18,10 +18,12 @@ const Skill = ({ name, x, y }) => {
 const Skills = () => {
   return (
     <>
-      <h2 className="font-bold text-8xl mt-64 w-full text-center">Skills</h2>
-      <div className="w-full h-screen flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark">
+      <h2 className="font-bold text-8xl mt-64 w-full text-center md:text-6xl md:mt-32">
+        Skills
+      </h2>
+      <div className="w-full h-screen flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark lg:h-[80vh] sm:h-[60vh] xs:h-[50vh] lg:bg-circularLightLg lg:dark:bg-circularDarkLg md:bg-circularLightMd md:dark:bg-circularDarkMd sm:bg-circularLightSm sm:dark:bg-circularDarkSm">
         <motion.div
-          className="flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark cursor-pointer dark:text-dark dark:bg-light"
+          className="flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark cursor-pointer dark:text-dark dark:bg-light lg:p-6 md:p-4 xs:text-xs xs:p-2"
           whileHover={{ scale: 1.05 }}
         >
           Web
@@ -31,9 +33,8 @@ const Skills = () => {
         <Skill name="JavaScript" x="20vw" y="6vw" />
         <Skill name="ReactJS" x="0vw" y="12vw" />
         <Skill name="NextJS" x="-20vw" y="-15vw" />
-        <Skill name="Figma" x="0vw" y="-20vw" />
-        <Skill name="Figma" x="0vw" y="-20vw" />
-        <Skill name="MaterialUI" x="15vw" y="-5vw" />
+        <Skill name="Figma" x="0vw" y="-24vw" />
+        <Skill name="MaterialUI" x="18vw" y="-5vw" />
         <Skill name="Tailwind CSS" x="15vw" y="-18vw" />
       </div>
     </>
