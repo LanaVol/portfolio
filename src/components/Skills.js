a1b9cbe50917b4a61282131bd87d1b6b0b1ea65d
LanaVol <svolosh4yk@gmail.com>
2023-08-13 23:35:31 +0300

    add skills icons

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { motion } from "framer-motion";
+import {
+  Figma,
+  ReactIcon,
+  MaterialUI,
+  CSSIcon,
+  TailwindIcon,
+  HTML,
+  JavaScript,
+} from "../components/Icons";
 
-const Skill = ({ name, x, y }) => {
+const Skill = ({ name, x, y, icon }) => {
   return (
     <motion.div
-      className="flex items-center justify-center rounded-full font-semibold bg-dark text-light py-3 px-6 shadow-dark cursor-pointer absolute dark:text-dark dark:bg-light lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3 xs:bg-transparent xs:dark:bg-transparent xs:text-dark xs:dark:text-light xs:font-bold"
+      className="flex items-center justify-center rounded-full font-semibold bg-dark text-light py-3 px-6 shadow-dark cursor-pointer absolute dark:text-dark dark:bg-light lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3 xs:bg-transparent  xs:dark:bg-transparent xs:text-dark xs:dark:text-light xs:font-bold"
       whileHover={{ scale: 1.05 }}
       initial={{ x: 0, y: 0 }}
       whileInView={{ x: x, y: y, transition: { duration: 1.5 } }}
       viewport={{ once: true }}
     >
-      {name}
+      <div className="mr-1">{icon}</div>
+      <div>{name}</div>
     </motion.div>
   );
 };
@@ -28,14 +38,15 @@ const Skills = () => {
         >
           Web
         </motion.div>
-        <Skill name="CSS" x="-5vw" y="-10vw" />
-        <Skill name="HTML" x="-20vw" y="2vw" />
-        <Skill name="JavaScript" x="20vw" y="6vw" />
-        <Skill name="ReactJS" x="0vw" y="12vw" />
-        <Skill name="NextJS" x="-20vw" y="-15vw" />
-        <Skill name="Figma" x="0vw" y="-24vw" />
-        <Skill name="MaterialUI" x="18vw" y="-5vw" />
-        <Skill name="Tailwind CSS" x="15vw" y="-18vw" />
+        <Skill name="CSS" x="0vw" y="-10vw" icon={<CSSIcon />} />
+        <Skill name="HTML" x="-15vw" y="5vw" icon={<HTML />} />
+        <Skill name="JavaScript" x="20vw" y="6vw" icon={<JavaScript />} />
+        <Skill name="ReactJS" x="0vw" y="12vw" icon={<ReactIcon />} />
+        <Skill name="TypeScript" x="-22vw" y="-5vw" />
+        <Skill name="NextJS" x="-15vw" y="-15vw" />
+        <Skill name="Figma" x="0vw" y="-24vw" icon={<Figma />} />
+        <Skill name="Material-UI" x="18vw" y="-5vw" icon={<MaterialUI />} />
+        <Skill name="Tailwind CSS" x="15vw" y="-18vw" icon={<TailwindIcon />} />
       </div>
     </>
   );
