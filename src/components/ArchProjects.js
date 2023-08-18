65a411c1e9a33878b82025cb55ed9862b37147da
LanaVol <svolosh4yk@gmail.com>
2023-08-18 23:33:54 +0300

    create components about arch projects&update

---
+++ src/components/ArchProjects.js
@@ -0,0 +1,88 @@
+import React from "react";
+import { useRef } from "react";
+import {
+  motion,
+  useScroll,
+  useSpring,
+  useTransform,
+  useInView,
+} from "framer-motion";
+import Image from "next/image";
+import project1 from "../../public/images/projects/task-manager-app.jpg";
+import project2 from "../../public/images/projects/pizza-family.jpg";
+import project3 from "../../public/images/projects/kettles-shop.jpg";
+
+function useParallax(value, distance) {
+  return useTransform(value, [0, 1], [-distance, distance]);
+}
+
+const FramerImage = motion(Image);
+
+function Imge({ id, img, title }) {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({ target: ref });
+  const y = useParallax(scrollYProgress, 300);
+  const isInView = useInView(ref, { once: true });
+
+  return (
+    <section className="h-[100vh] flex justify-center items-center relative snap-center">
+      <div
+        ref={ref}
+        className="w-[900px] h-[500px] relative m-5 max-h-[90vh] bg-white overflow-hidden"
+      >
+        <FramerImage
+          src={img}
+          alt="alt"
+          className="w-full h-auto"
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.2 }}
+          priority
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
+        />
+
+        {/* <Image
+          src={img}
+          alt="Arch project"
+          priority
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          className="absolute top-0 left-0 right-0 bottom-0 w-full h-auto"
+        /> */}
+      </div>
+      <motion.h2
+        style={{
+          transform: isInView ? "none" : "translateX(-200px)",
+          opacity: isInView ? 1 : 0,
+          transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+        }}
+        className="m-0 left-0 text-[60px] font-bold absolute"
+      >
+        {title}
+      </motion.h2>
+      <motion.h2
+        style={{ y }}
+        className="m-0 left-[calc(70%+130px)] text-[80px] font-bold absolute"
+      >{`#00${id}`}</motion.h2>
+    </section>
+  );
+}
+const ArchProjects = () => {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
+  return (
+    <>
+      <Imge id={1} img={project1} title={"Intergal City"} />
+      <Imge id={2} img={project2} title={"Business center S26"} />
+      <Imge id={3} img={project3} title={"Volodymyrkiy"} />
+      <motion.div
+        className="fixed left-0 right-0 bottom-[98px] bg-primary h-1"
+        style={{ scaleX }}
+      />
+    </>
+  );
+};
+
+export default ArchProjects;
--- src/pages/articles.js
+++ src/pages/articles.js
@@ -9,100 +9,101 @@ import article2 from "../../public/images/articles/create loading screen in reac
 import article3 from "../../public/images/articles/create modal component in react using react portals.png";
 import article4 from "../../public/images/articles/form validation in reactjs using custom react hook.png";
 import article5 from "../../public/images/articles/smooth scrolling in reactjs.png";
-
 import { motion, useMotionValue } from "framer-motion";
 import TransitionEffect from "@/components/TransitionEffect";
 
+import ArchProjects from "@/components/ArchProjects";
+
 const FramerImage = motion(Image);
 
-const MovingImg = ({ title, img, link }) => {
-  const x = useMotionValue(0);
-  const y = useMotionValue(0);
-  const imgRef = useRef(null);
+// const MovingImg = ({ title, img, link }) => {
+//   const x = useMotionValue(0);
+//   const y = useMotionValue(0);
+//   const imgRef = useRef(null);
 
-  function handleMouse(event) {
-    imgRef.current.style.display = "inline-block";
-    x.set(event.pageX);
-    y.set(-10);
-  }
+//   function handleMouse(event) {
+//     imgRef.current.style.display = "inline-block";
+//     x.set(event.pageX);
+//     y.set(-10);
+//   }
 
-  function handleMouseLeave(event) {
-    imgRef.current.style.display = "none";
-    x.set(0);
-    y.set(0);
-  }
+//   function handleMouseLeave(event) {
+//     imgRef.current.style.display = "none";
+//     x.set(0);
+//     y.set(0);
+//   }
 
-  return (
-    <Link
-      href={link}
-      target="_blank"
-      onMouseMove={handleMouse}
-      onMouseLeave={handleMouseLeave}
-    >
-      <h2 className="capitalize text-xl font-semibold hover:underline">
-        {title}
-      </h2>
-      <FramerImage
-        style={{ x: x, y: y }}
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1, transition: { duration: 0.3 } }}
-        ref={imgRef}
-        src={img}
-        alt={title}
-        className="z-10 w-96 h-auto hidden absolute rounded-lg md:!hidden"
-      />
-    </Link>
-  );
-};
+//   return (
+//     <Link
+//       href={link}
+//       target="_blank"
+//       onMouseMove={handleMouse}
+//       onMouseLeave={handleMouseLeave}
+//     >
+//       <h2 className="capitalize text-xl font-semibold hover:underline">
+//         {title}
+//       </h2>
+//       <FramerImage
+//         style={{ x: x, y: y }}
+//         initial={{ opacity: 0 }}
+//         whileInView={{ opacity: 1, transition: { duration: 0.3 } }}
+//         ref={imgRef}
+//         src={img}
+//         alt={title}
+//         className="z-10 w-96 h-auto hidden absolute rounded-lg md:!hidden"
+//       />
+//     </Link>
+//   );
+// };
 
-const Article = ({ img, title, date, link }) => {
-  return (
-    <motion.li
-      initial={{ y: 200 }}
-      whileInView={{ y: 0, transition: { duration: 0.5, ease: "easeInOut" } }}
-      viewport={{ once: true }}
-      className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4 dark:border-light dark:bg-dark dark:text-light sm:flex-col"
-    >
-      <MovingImg title={title} link={link} img={img} />
-      <span className="text-primary font-semibold pl-4 dark:text-primaryDark sm:self-start sm:pl-0 xs:text-sm">
-        {date}
-      </span>
-    </motion.li>
-  );
-};
+// const Article = ({ img, title, date, link }) => {
+//   return (
+//     <motion.li
+//       initial={{ y: 200 }}
+//       whileInView={{ y: 0, transition: { duration: 0.5, ease: "easeInOut" } }}
+//       viewport={{ once: true }}
+//       className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4 dark:border-light dark:bg-dark dark:text-light sm:flex-col"
+//     >
+//       <MovingImg title={title} link={link} img={img} />
+//       <span className="text-primary font-semibold pl-4 dark:text-primaryDark sm:self-start sm:pl-0 xs:text-sm">
+//         {date}
+//       </span>
+//     </motion.li>
+//   );
+// };
 
-const FeaturedActicle = ({ img, title, time, summary, link }) => {
-  return (
-    <li className="col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl relative dark:bg-dark dark:border-light">
-      <div className="absolute top-0 -right-3 -z-10 w-[101.5%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl dark:bg-light" />
-      <Link
-        href={link}
-        target="_blank"
-        className="w-full inline-block cursor-pointer overflow-hidden rounded-lg"
-      >
-        <FramerImage
-          src={img}
-          alt={title}
-          className="w-full h-auto"
-          whileHover={{ scale: 1.05 }}
-          transition={{ duration: 0.2 }}
-          priority
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
-        />
-      </Link>
+// const FeaturedActicle = ({ img, title, time, summary, link }) => {
+//   return (
+//     <li className="col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl relative dark:bg-dark dark:border-light">
+//       <div className="absolute top-0 -right-3 -z-10 w-[101.5%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl dark:bg-light" />
+//       <Link
+//         href={link}
+//         target="_blank"
+//         className="w-full inline-block cursor-pointer overflow-hidden rounded-lg"
+//       >
+//         <FramerImage
+//           src={img}
+//           alt={title}
+//           className="w-full h-auto"
+//           whileHover={{ scale: 1.05 }}
+//           transition={{ duration: 0.2 }}
+//           priority
+//           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
+//         />
+//       </Link>
 
-      <Link href={link} target="_blank">
-        <h2 className="capitalize text-2xl font-bold my-2 hover:underline mt-4 xs:text-lg">
-          {title}
-        </h2>
-        <p className="text-sm mb-2">{summary}</p>
-        <span className="text-primary font-semibold dark:text-primaryDark">
-          {time}
-        </span>
-      </Link>
-    </li>
-  );
-};
+//       <Link href={link} target="_blank">
+//         <h2 className="capitalize text-2xl font-bold my-2 hover:underline mt-4 xs:text-lg">
+//           {title}
+//         </h2>
+//         <p className="text-sm mb-2">{summary}</p>
+//         <span className="text-primary font-semibold dark:text-primaryDark">
+//           {time}
+//         </span>
+//       </Link>
+//     </li>
+//   );
+// };
 
 const articles = () => {
   return (
@@ -119,8 +120,9 @@ const articles = () => {
             text="Words Can Change The World!"
             className="mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl"
           />
+          <ArchProjects />
 
-          <ul className="grid grid-cols-2 gap-16 lg:gap-8 md:grid-cols-1 md:gap-y-16">
+          {/* <ul className="grid grid-cols-2 gap-16 lg:gap-8 md:grid-cols-1 md:gap-y-16">
             <FeaturedActicle
               title="Build A Custom Pagination Component In Reactjs From Scratch"
               summary="Learn how to build a custom pagination component in ReactJS from scratch. Follow this step-by-step guide to integrate Pagination component in your ReactJS project."
@@ -135,9 +137,9 @@ const articles = () => {
               link="/"
               img={article2}
             />
-          </ul>
+          </ul> */}
 
-          <h2 className="font-bold text-4xl w-full text-center my-16 mt-32">
+          {/* <h2 className="font-bold text-4xl w-full text-center my-16 mt-32">
             All articles
           </h2>
 
@@ -166,7 +168,7 @@ const articles = () => {
               link="/"
               img={article3}
             />
-          </ul>
+          </ul> */}
         </Layout>
       </main>
     </>
