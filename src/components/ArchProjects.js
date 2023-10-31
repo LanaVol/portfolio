6a9ce42dcca073966e4b98b4468bce431d4745ff
LanaVol <svolosh4yk@gmail.com>
2023-10-31 20:26:50 +0200

    improve & update

--- src/components/ArchProjects.js
+++ src/components/ArchProjects.js
@@ -1,77 +1,41 @@
 import React from "react";
-import { useRef } from "react";
-import {
-  motion,
-  useScroll,
-  useSpring,
-  useTransform,
-  useInView,
-} from "framer-motion";
-import Image from "next/image";
-import Link from "next/link";
+import { motion, useScroll, useSpring } from "framer-motion";
 import project1 from "../../public/images/projects/intergalCity.jpg";
 import project2 from "../../public/images/projects/s26.jpg";
 import project3 from "../../public/images/projects/volodymyrsky.jpg";
 import project4 from "../../public/images/projects/malahit-5.jpg";
+import { ProjectBlock } from "./ProjectBlock";
 
-function useParallax(value, distance) {
-  return useTransform(value, [0, 1], [-distance, distance]);
-}
-
-const FramerImage = motion(Image);
-
-function Imge({ id, img, title, link, subtitle }) {
-  const ref = useRef(null);
-  const { scrollYProgress } = useScroll({ target: ref });
-  const y = useParallax(scrollYProgress, 300);
-  const isInView = useInView(ref, { once: true });
-
-  return (
-    <section className="h-[90vh] flex justify-center items-center snap-center relative">
-      <div
-        className="w-[1000px] min-h-[600px] max-h-[90vh] flex items-center justify-center rounded-br-2xl rounded-3xl border border-solid border-dark  bg-light shadow-2xl p-12 relative dark:bg-dark dark:border-light lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4"
-        ref={ref}
-      >
-        <div className="absolute top-0 -right-3 -z-10 w-[100.5%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl dark:bg-light xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]" />
-
-        <Link
-          href={link}
-          target="_blank"
-          className="h-full w-auto cursor-pointer overflow-hidden rounded-lg lg:w-full "
-        >
-          <FramerImage
-            src={img}
-            alt={title}
-            className="w-full h-auto"
-            // className="w-auto h-full m-auto rounded-2xl"
-            whileHover={{ scale: 1.05 }}
-            transition={{ duration: 0.2 }}
-            priority
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
-          />
-        </Link>
-
-        <motion.div
-          style={{
-            transform: isInView ? "none" : "translateX(-250px)",
-            opacity: isInView ? 1 : 0,
-            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
-          }}
-          className="absolute left-[-50px] top-[-150px] sm:left-2 sm:top-8"
-        >
-          <h2 className="m-0 text-[50px] font-bold md:text-[24px]">{title}</h2>
-          <p className="m-0 text-[20px] font-bold text-primary  xs:text-base">
-            {subtitle}
-          </p>
-        </motion.div>
-        <motion.h2
-          style={{ y }}
-          className="m-0 left-[calc(70%+40px)] text-[80px] font-bold absolute sm:left-[calc(70%-50px)] text-primary"
-        >{`#00${id}`}</motion.h2>
-      </div>
-    </section>
-  );
-}
+const arrArchProjects = [
+  {
+    id: 1,
+    img: project1,
+    title: "Intergal City",
+    subtitle: "Multifunctional Complex | Under Construction",
+    link: "https://intergal.city/ua/about/concept/",
+  },
+  {
+    id: 2,
+    img: project2,
+    title: "S26",
+    subtitle: "Business Center | Construction completed",
+    link: "https://office.com.ua/business-centers/s26",
+  },
+  {
+    id: 3,
+    img: project4,
+    title: "Malahyt №5, №4(partly)",
+    subtitle: "Residential Complex | Construction completed",
+    link: "https://intergal-bud.com.ua/completed-projects/kyiv",
+  },
+  {
+    id: 4,
+    img: project3,
+    title: "Volodymyrskiy",
+    subtitle: "Residential Complex | Construction completed",
+    link: "https://volodymyrskyy.com.ua/#close",
+  },
+];
 
 const ArchProjects = () => {
   const { scrollYProgress } = useScroll();
@@ -82,34 +46,16 @@ const ArchProjects = () => {
   });
   return (
     <>
-      <Imge
-        id={1}
-        img={project1}
-        title={"Intergal City"}
-        subtitle={"Multifunctional Complex | Under Construction"}
-        link={"https://intergal.city/ua/about/concept/"}
-      />
-      <Imge
-        id={2}
-        img={project2}
-        title={"S26"}
-        subtitle={"Business Center | Construction completed"}
-        link={"https://office.com.ua/business-centers/s26"}
-      />
-      <Imge
-        id={3}
-        img={project4}
-        title={"Malahyt №5, №4(partly)"}
-        subtitle={"Residential Complex | Construction completed"}
-        link={"https://intergal-bud.com.ua/completed-projects/kyiv"}
-      />
-      <Imge
-        id={4}
-        img={project3}
-        title={"Volodymyrskiy"}
-        subtitle={"Residential Complex | Construction completed"}
-        link={"https://volodymyrskyy.com.ua/#close"}
-      />
+      {arrArchProjects.map((el) => (
+        <ProjectBlock
+          key={el.id}
+          id={el.id}
+          img={el.img}
+          title={el.title}
+          subtitle={el.subtitle}
+          link={el.link}
+        />
+      ))}
       <motion.div
         className="fixed left-0 right-0 bottom-[98px] bg-primary h-1"
         style={{ scaleX }}
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -15,67 +15,17 @@ import project6 from "../../public/images/projects/projectHome.jpg";
 import project7 from "../../public/images/projects/barbershop.jpg";
 import { motion } from "framer-motion";
 import TransitionEffect from "@/components/TransitionEffect";
+import { MainProject } from "@/components/MainProject";
+import { BlockShadow } from "@/components/BlockShadow";
 
 const FramerImage = motion(Image);
-
-const FeaturedProject = ({ type, title, summary, img, link, github }) => {
-  return (
-    <article className="w-full flex items-center justify-between rounded-br-2xl rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 relative dark:bg-dark dark:border-light lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4">
-      <div className="absolute top-0 -right-3 -z-10 w-[100.5%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl dark:bg-light xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]" />
-      <Link
-        href={link}
-        target="_blank"
-        className="w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full"
-      >
-        <FramerImage
-          src={img}
-          alt={title}
-          className="w-full h-auto"
-          whileHover={{ scale: 1.05 }}
-          transition={{ duration: 0.2 }}
-          priority
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
-        />
-      </Link>
-
-      <div className="w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6">
-        <span className="text-primary font-medium text-xl dark:text-primaryDark xs:text-base">
-          {type}
-        </span>
-        <Link
-          href={link}
-          target="_blank"
-          className="hover:underline underline-offset-2"
-        >
-          <h2 className="my-2 w-full text-left text-4xl font-bold dark:text-light sm:text-sm">
-            {title}
-          </h2>
-        </Link>
-        <p className="my-2 font-medium text-dark dark:text-light sm:text-sm">
-          {summary}
-        </p>
-
-        <div className="mt-2 flex items-center">
-          <Link href={github} target="_blank" className="w-10">
-            <GithubIcon />
-          </Link>
-          <Link
-            href={link}
-            target="_blank"
-            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold dark:bg-light dark:text-dark sm:px-4 sm:text-base"
-          >
-            Visit Project
-          </Link>
-        </div>
-      </div>
-    </article>
-  );
-};
 
 const Project = ({ title, type, img, link, github, Component }) => {
   return (
     <article className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light">
-      <div className="absolute top-0 -right-3 -z-10 w-[101.5%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl dark:bg-light md:-right-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]" />
+      <BlockShadow
+        className={"w-[101.5%] md:-right-2 md:w-[101%] xs:h-[102%]"}
+      />
       <Link
         href={link}
         target="_blank"
@@ -138,7 +88,7 @@ const projects = () => {
 
           <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
             <div className="col-span-12">
-              <FeaturedProject
+              <MainProject
                 title="Events Application (under revision)"
                 img={project11}
                 summary="The website shows the information about various exciting events happening around the world. Here, you can explore a diverse range of events, from concerts and exhibitions to sports competitions and cultural festivals. On this website, you will discover an up-to-date event calendar featuring detailed descriptions, dates, venues, and other valuable information."
@@ -171,7 +121,7 @@ const projects = () => {
             </div>
 
             <div className="col-span-12">
-              <FeaturedProject
+              <MainProject
                 title="Task-Manager Application"
                 img={project1}
                 summary="A feature-rich Task-Manager App where you can organize tasks with the ability to distribute them by topic in your opinion. There are functions for add, remove and change cards and bords. Each card has several fields of information: title, description, date, label and tasks with remove and complete status. Also there are two moods: light and dark. Invite you to log in with test-user data or you can register yourself."
@@ -209,7 +159,7 @@ const projects = () => {
                 summary=""
                 link="https://project-home-rho.vercel.app/"
                 github="https://github.com/LanaVol/project-home"
-                type="JavaScript, HTML, CSS(SCSS), AOS-Animation"
+                type="JavaScript, HTML, CSS(SCSS), Webpack(basic configuration), AOS-Animation"
                 Component={<GithubIcon />}
               />
             </div>
