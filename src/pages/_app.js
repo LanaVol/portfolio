8568147b71f7b909ef0f7ed87510c8c1308a9f77
LanaVol <svolosh4yk@gmail.com>
2024-01-07 23:52:57 +0200

    add analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,7 @@ import { Montserrat } from "next/font/google";
 import Head from "next/head";
 import { AnimatePresence } from "framer-motion";
 import { useRouter } from "next/router";
+import { Analytics } from "@vercel/analytics/react";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -27,6 +28,7 @@ export default function App({ Component, pageProps }) {
           <Component key={router.asPath} {...pageProps} />
         </AnimatePresence>
         <Footer />
+        <Analytics />
       </main>
     </>
   );
